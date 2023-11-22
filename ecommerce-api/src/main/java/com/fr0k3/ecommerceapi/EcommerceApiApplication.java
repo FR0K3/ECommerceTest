95a7ff11b8860b9cb4ceaa10c78e817637700913
FR0K3 <00174320@uca.edu.sv>
2023-11-22 10:58:00 -0600

    First commit

---
+++ ecommerce-api/src/main/java/com/fr0k3/ecommerceapi/EcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.fr0k3.ecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApiApplication.class, args);
+    }
+
+}
---
+++ ecommerce-api/src/test/java/com/fr0k3/ecommerceapi/EcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr0k3.ecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
